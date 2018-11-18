4b438c3b3a79abb073a028db194eadd33611df1b
eduh1997 <eduardooliveira.1997@alunos.utfpr.edu.br>
2018-11-18 17:51:59 -0200

    generating db

---
+++ generateDB.py
@@ -0,0 +1,18 @@
+import wikipedia
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+client.drop_database("Wiki-Graph")
+db = client["Wiki-Graph"]
+
+main_page = "Computer Science"
+
+links_1nd_degree = wikipedia.WikipediaPage(main_page).links
+for link in links_1nd_degree:
+    try:
+        links_2nd_degree = wikipedia.WikipediaPage(link).links
+        link = link.replace(" ", "_")
+        for links_2 in links_2nd_degree:
+            db[link].insert_one({'name':links_2})
+        print link + " collect"
+    except:
+        print "Error in " + link
